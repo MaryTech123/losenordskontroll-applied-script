0c622d29eeb165270786e84c539190fb421647fa
Maria <maria.mhj86@gmail.com>
2026-01-06 07:52:59 -0500

    Lade till kontroll nummer 2 som kontrollerar om det finns stor bokstav i lösenordet. + några små ändringar i tidigare koden(vad som visas i print, osv)

--- losenordskontroll.py
+++ losenordskontroll.py
@@ -13,7 +13,14 @@ def kontrollera_losenord(losenord):
         print ("FEL: lösenordet är kortare än 12 tecken")
         starkt= False
 
+# Nu gör jag kontroll 2: om det finns stor bokstav i lösenordet. islower är TRUE om alla bokstäver är små, då betyder det att det finns ingen stor bokstav.
+    if losenord.islower():
+      print ("FEL: ditt lösenord innehåller ingen stor bokstav.")
+      starkt= False
+
+
     return starkt
+
 
 #Skriver rubriken
 print ("Kontroll av ditt lösenord:")
@@ -27,11 +34,9 @@ starkt = kontrollera_losenord(losenord)
 #Skriver ut rubrik för resultaten:
 print ("Resultat: ")
 
-#Om starkt är TRUE, kontrollen klarades
+#Om starkt är TRUE, kontrollerna klarades
 if starkt:
-        print ("Lösenordet är Ok!")
+        print ("Lösenordet är Godkänt!!")
 else: # Om Starkt är FALSE, kontrollen mislyckades och lösenordet är svag.
-	print ("Lösenordet är SVAGT.")
-
-
+	print ("Lösenordet är SVAGT, du borde ändra det.")
 
